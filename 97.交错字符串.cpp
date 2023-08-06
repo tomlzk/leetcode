d39855af4800fc2e2c291dcf141c144e7c7427ca
tomlzk <lulu00618@gmail.com>
2023-08-06 18:41:38 +0800

    97，119

--- 97.交错字符串.cpp
+++ 97.交错字符串.cpp
@@ -5,26 +5,37 @@
  */
 
 // @lc code=start
-#include<iostream>
+#include <iostream>
 using namespace std;
-#include<vector>
+#include <vector>
 class Solution
 {
 public:
-    bool isInterleave(string s1, string s2, string s3)
-    {
-        int i = 0;
-        int j = 0;
-        int m = 0;
-        int n = 0;
-        int k = 0;
-        for(; k < s3.size(); k++)
-        {
-            if(s1[i] == s3[k])
-            {
-                i++;
+    bool isInterleave(string s1, string s2, string s3) {
+        auto f = vector < vector <int> > (s1.size() + 1, vector <int> (s2.size() + 1, false));
+
+        int n = s1.size(), m = s2.size(), t = s3.size();
+
+        if (n + m != t) {
+            return false;
+        }
+
+        f[0][0] = true;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[i][j] |= (f[i - 1][j] && s1[i - 1] == s3[p]);
+                }
+                if (j > 0) {
+                    f[i][j] |= (f[i][j - 1] && s2[j - 1] == s3[p]);
+                }
             }
         }
+
+        return f[n][m];
     }
+
+
 };
 // @lc code=end
