6a3ea60ecf8fd94547362bb30536d5f5bb688a1c
tomlzk <lulu00618@gmail.com>
2023-08-09 22:00:33 +0800

    122

---
+++ 123.买卖股票的最佳时机-iii.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=cpp
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int i = 0;
+        vector<vector<int>> res;
+        res.resize(prices.size());
+        res[0].resize(2);
+        res[0][0] = 0; res[0][1] = - prices[0];
+        for(i = 1; i < prices.size(); i++)
+        {
+            res[i].resize(2);
+            res[i][0] = max(res[i - 1][0], res[i - 1][1] + prices[i]);
+            res[i][1] = max(res[i - 1][0] - prices[i], res[i - 1][1]);
+        }
+        i--;
+        return max(res[i][0], res[i][1]);
+    }
+};
+// @lc code=end
+
