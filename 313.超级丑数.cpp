1bf931401ab32d0ad825b48bc921ac04794c2568
tomlzk <lulu00618@gmail.com>
2023-09-01 15:45:42 +0800

    313c++

---
+++ 313.超级丑数.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=313 lang=cpp
+ *
+ * [313] 超级丑数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        vector<long> dp(n + 1);
+        int m = primes.size();
+        vector<int> pointers(m, 0);
+        vector<long> nums(m, 1);
+        for (int i = 1; i <= n; i++) {
+            long minNum = INT_MAX;
+            for (int j = 0; j < m; j++) {
+                minNum = min(minNum, nums[j]);
+            }
+            dp[i] = minNum;
+            for (int j = 0; j < m; j++) {
+                if (nums[j] == minNum) {
+                    pointers[j]++;
+                    nums[j] = dp[pointers[j]] * primes[j];
+                }
+            }
+        }
+        return dp[n];
+    }
+};
+// @lc code=end
+
