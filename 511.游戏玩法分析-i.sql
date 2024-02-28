a586cfef9508f29a498e4ce2652fd27ab1c9f03a
tomlzk <lulu00618@gmail.com>
2024-02-28 22:37:57 +0800

    no have comment

---
+++ 511.游戏玩法分析-i.sql
@@ -0,0 +1,17 @@
+--
+-- @lc app=leetcode.cn id=511 lang=mysql
+--
+-- [511] 游戏玩法分析 I
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT
+  A.player_id,
+  MIN(A.event_date) AS first_login
+FROM
+  Activity A
+GROUP BY
+  A.player_id;
+-- @lc code=end
+
