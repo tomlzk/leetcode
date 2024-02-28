11cb3d38becfeecf1832b313199bf6aaf791a069
tomlzk <lulu00618@gmail.com>
2024-02-28 14:15:20 +0800

    94

--- 94.二叉树的中序遍历.cpp
+++ 94.二叉树的中序遍历.cpp
@@ -19,11 +19,37 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+//vector<int> result;
 class Solution
 {
 public:
+    vector<int> result;
+    void mediumTraver(TreeNode *root)
+    {
+        if(root == NULL)
+        {
+            return ;
+        }
+        mediumTraver(root->left);
+        result.push_back(root->val);
+        mediumTraver(root->right);
+    }
+
     vector<int> inorderTraversal(TreeNode *root)
     {
+        mediumTraver(root);
+        return result;
     }
 };
 // @lc code=end
