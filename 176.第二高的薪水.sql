f43a0ba9fabd2a31469c6670d4b0ffadb966520b
tomlzk <lulu00618@gmail.com>
2024-03-01 19:05:59 +0800

    database

---
+++ 176.第二高的薪水.sql
@@ -0,0 +1,18 @@
+--
+-- @lc app=leetcode.cn id=176 lang=mysql
+--
+-- [176] 第二高的薪水
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+-- @lc code=end
+
