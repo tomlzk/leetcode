5fdf7ea64146360614546556f019212d6316e7f8
tomlzk <lulu00618@gmail.com>
2024-03-01 21:11:16 +0800

    dabebase dense_rank over(partition by *  order by *)

---
+++ 178.分数排名.sql
@@ -0,0 +1,11 @@
+--
+-- @lc app=leetcode.cn id=178 lang=mysql
+--
+-- [178] 分数排名
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select score, dense_rank() over (order by score desc) as 'rank' from Scores;
+-- @lc code=end
+
