6ce7eb4507e49c738cb7dec459487801375c81a2
tomlzk <lulu00618@gmail.com>
2024-03-02 23:25:18 +0800

    python3 map deep copy

---
+++ 133.克隆图.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=133 lang=python3
+#
+# [133] 克隆图
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        return deepcopy(node)
+        
+# @lc code=end
+
