11724900ea6195fcb67479168f628b23934b2169
tomlzk <lulu00618@gmail.com>
2024-03-04 16:58:42 +0800

    c++-lookBack

---
+++ 77.组合.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=cpp
+ *
+ * [77] 组合
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+#include <vector>
+class Solution
+{
+public:
+    vector<vector<int>> result;
+
+    void back(vector<int> res, int i, int n, int k)
+    {
+        if(res.size() == k)
+        {
+            result.push_back(res);
+            //res.pop_back();
+            // back(res, i + 1, n, k);
+            return;
+        }
+        if(i > n)
+        {
+            return ;
+        }
+
+        res.push_back(i);
+        back(res, i + 1, n, k);
+        res.pop_back();
+        back(res, i + 1, n, k);
+
+    
+
+    }
+
+    vector<vector<int>> combine(int n, int k)
+    {
+        vector<int> res;
+        // for(int i = 1; i <= n; i++)
+        // {
+        //     res.push_back(i);
+        //     back(res, i + 1, n, k);
+        //     res.pop_back();
+
+        // }
+        back(res, 1, n, k);
+        return result;
+    }
+};
+// @lc code=end
