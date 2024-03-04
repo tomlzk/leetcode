7501a0d62edab0a5b8f7e50800439996c2807603
tomlzk <lulu00618@gmail.com>
2024-03-04 19:09:38 +0800

    sliding windows

---
+++ 219.存在重复元素-ii.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=cpp
+ *
+ * [219] 存在重复元素 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        int length = nums.size();
+        for (int i = 0; i < length; i++) {
+            if (i > k) {
+                s.erase(nums[i - k - 1]);
+            }
+            if (s.count(nums[i])) {
+                return true;
+            }
+            s.emplace(nums[i]);
+        }
+        return false;
+    }
+};
+// @lc code=end
+
